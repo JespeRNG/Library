bb734249bb77ca64fa18b0afeedbca9ae2000608
polishchuk37a <polishchuk.andrey2002@gmail.com>
2021-02-24 18:26:00 +0200

    Added Add window

--- Library/Library/MainWindow.xaml.cs
+++ Library/Library/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Configuration;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,15 +13,20 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
 
 namespace Library
 {
     public partial class MainWindow : Window
     {
+        AddWindow addWindow = new AddWindow();
         public MainWindow()
         {
             InitializeComponent();
 
+            
             /*using (var context = new MyDbContext())
             {
                 var reader = new Reader()
@@ -34,7 +40,30 @@ namespace Library
 
                 context.Readers.Add(reader);
                 context.SaveChanges();
+
+                
             }*/
+
+            bindDataGrid();
+         }
+
+        private void bindDataGrid()
+        {
+            SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["DbConnectionString"].ConnectionString;
+            con.Open();
+            SqlCommand cmd = new SqlCommand("SELECT * FROM [Readers]", con);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable("Readers");
+            adapter.Fill(dt);
+            con.Close();
+
+            ReadersGrid.ItemsSource = dt.DefaultView;
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {   
+            addWindow.Show();
         }
     }
 }
